94010218e57893dc1a2767e8974ad54604a9220d
diegodovale21 <diegodovale21@gmail.com>
2022-12-28 22:31:47 -0500

    Adds integrations database SQL

--- api/turma/resolvers/turmaResolvers.js
+++ api/turma/resolvers/turmaResolvers.js
@@ -1,19 +1,6 @@
-const turmas = [
-  {
-    id: 1,
-    descricao: "Básico"
-
-  },
-  {
-    id: 2,
-    descricao: "Intermediário"
-
-  }
-]
-
 const turmaResolvers = {
   Query: {
-    turmas: (parent, args, context, info) => turmas
+    turmas: (_, __, {dataSources}) => dataSources.turmasAPI.getTurmas()
   }
 }
 
